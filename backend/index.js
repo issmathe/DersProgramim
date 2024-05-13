accbfd3f1b2a2b8b05b510fb0f4d3bf1af4e34cd
ahmet eraslan <111170058+issmathe@users.noreply.github.com>
2024-05-13 23:31:57 +0300

    düzeltme

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: 'https://dersprogramim.vercel.app' }));
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
