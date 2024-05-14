5d79315bfc3f5e8ff57e3f3b77f612164efaeace
ahmet eraslan <111170058+issmathe@users.noreply.github.com>
2024-05-14 22:40:49 +0300

    adfsdf

--- frontend/src/pages/kayitEkleme/KayitEkleme.jsx
+++ frontend/src/pages/kayitEkleme/KayitEkleme.jsx
@@ -1,65 +1,32 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from "../../components/header/Header";
 
 const KayitEkleme = () => {
-  const [name, setName] = useState("");
-  const [surname, setSurname] = useState("");
-  const [idDegeri, setIdDegeri] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const [successMessage, setSuccessMessage] = useState("");
+  const [users, setUsers] = useState([]);
 
-  const handleNameChange = (event) => {
-    setName(event.target.value);
-  };
-
-  const handleSurnameChange = (event) => {
-    setSurname(event.target.value);
-  };
-
-  const handleIdDegeriChange = (event) => {
-    setIdDegeri(event.target.value);
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    try {
-      const response = await axios.post("http://localhost:5000/api/users/add", {
-        name,
-        surname,
-        idDegeri
+  useEffect(() => {
+    // Tüm kullanıcıları almak için API'ye istek yap
+    axios.get('http://localhost:5000/api/users/get-all')
+      .then(response => {
+        setUsers(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching users: ', error);
       });
-      
-      setSuccessMessage("Kayıt başarıyla eklendi!");
-      setName("");
-      setSurname("");
-      setIdDegeri("");
-    } catch (error) {
-      setErrorMessage("Kayıt eklenirken bir hata oluştu.");
-      console.error(error);
-    }
-  };
+  }, []); // Boş bağımlılık dizisi, yalnızca bir kez yürütülmesini sağlar
 
   return (
     <div>
-      <h2>Kayıt Ekleme</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Ad:</label>
-          <input type="text" value={name} onChange={handleNameChange} required />
-        </div>
-        <div>
-          <label>Soyad:</label>
-          <input type="text" value={surname} onChange={handleSurnameChange} required />
-        </div>
-        <div>
-          <label>ID Değeri:</label>
-          <input type="text" value={idDegeri} onChange={handleIdDegeriChange} required />
-        </div>
-        <button type="submit">Kaydet</button>
-      </form>
-      {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
-      {successMessage && <p style={{ color: "green" }}>{successMessage}</p>}
+        <Header/>
+      <h2>Kullanıcılar</h2>
+      <ul>
+        {users.map(user => (
+          <li key={user.idDegeri}>
+            Adı: {user.username}  ID: {user._id}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
