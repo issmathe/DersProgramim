5a39d814a0ac42de3d03d9cf2a33732b55005871
ahmet eraslan <111170058+issmathe@users.noreply.github.com>
2024-05-16 22:32:34 +0300

    kod güncelleme

--- frontend/src/pages/hakkimizda/Hakkimizda.jsx
+++ frontend/src/pages/hakkimizda/Hakkimizda.jsx
@@ -4,20 +4,21 @@ import Header from "../../components/header/Header";
 import { message } from "antd";
 
 const Hakkimizda = () => {
-  const [data, setData] = useState([]);
-  const [formData, setFormData] = useState({ name: "", surname: "" });
-  const [loggedInUserId, setLoggedInUserId] = useState("");
+  const [data, setData] = useState([]); // Veri durumu
+  const [formData, setFormData] = useState({ name: "", surname: "" }); // Form veri durumu
+  const [loggedInUserId, setLoggedInUserId] = useState(""); // Giriş yapan kullanıcının kimliği
 
   useEffect(() => {
-    fetchData();
+    fetchData(); // Verileri almak için useEffect kullanımı
     const loggedInUser = localStorage.getItem("posUser");
     if (loggedInUser) {
-      fetchLoggedInUserId(loggedInUser);
+      fetchLoggedInUserId(loggedInUser); // Giriş yapan kullanıcının kimliğini almak için useEffect kullanımı
     } else {
-      message.error("Giriş yapmadınız!!");
+      message.error("Giriş yapmadınız!!"); // Hata mesajı
     }
   }, []);
 
+  // Giriş yapan kullanıcının kimliğini almak için fonksiyon
   const fetchLoggedInUserId = async (loggedInUser) => {
     try {
       const response = await fetch("http://localhost:5000/api/users/get-all");
@@ -30,47 +31,51 @@ const Hakkimizda = () => {
       if (foundUser) {
         setLoggedInUserId(foundUser._id);
       } else {
-        message.error("Kullanıcı veritabanında bulunamadı!");
+        message.error("Kullanıcı veritabanında bulunamadı!"); // Hata mesajı
       }
     } catch (error) {
-      console.error("Error fetching logged in user id:", error);
+      console.error("Error fetching logged in user id:", error); // Hata mesajı
     }
   };
 
+  // Verileri almak için fonksiyon
   const fetchData = async () => {
     try {
       const response = await axios.get(
         process.env.REACT_APP_SERVER_URL + "/api"
       );
-      setData(response.data);
+      setData(response.data); // Verileri ayarla
     } catch (error) {
-      console.error("Error fetching data:", error);
+      console.error("Error fetching data:", error); // Hata mesajı
     }
   };
 
+  // Form değerlerini güncellemek için fonksiyon
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
+  // Form gönderildiğinde çalışan fonksiyon
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const id = loggedInUserId; // Giriş yapan kullanıcının ID'sini al
-      const newFormData = { ...formData, id }; // Kullanıcının ID'si ile formData'yı güncelle
+      const id = loggedInUserId; // Giriş yapan kullanıcının kimliğini al
+      const newFormData = { ...formData, id }; // Kullanıcının kimliği ile formData'yı güncelle
       await axios.post(process.env.REACT_APP_SERVER_URL + "/api", newFormData);
-      fetchData();
-      setFormData({ name: "", surname: "" });
+      fetchData(); // Verileri yeniden al
+      setFormData({ name: "", surname: "" }); // Form verilerini temizle
     } catch (error) {
-      console.error("Error adding data:", error);
+      console.error("Error adding data:", error); // Hata mesajı
     }
   };
 
+  // Öğeyi silmek için fonksiyon
   const handleDelete = async (itemId) => {
     try {
       await axios.delete(process.env.REACT_APP_SERVER_URL + `/api/${itemId}`);
-      fetchData();
+      fetchData(); // Verileri yeniden al
     } catch (error) {
-      console.error("Error deleting data:", error);
+      console.error("Error deleting data:", error); // Hata mesajı
     }
   };
 
